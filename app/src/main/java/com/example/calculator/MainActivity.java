3332d84b8e012cfd2431937af07286ce85d3bd7d
Adam <aalcorn@stu.jsu.edu>
2019-02-24 23:18:26 -0600

    Progress

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,8 +8,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
+    double firstOperand = 0;
+    double secondOperand = 0;
+    String operation = "";
+    String currentEntry = "";
+    String displayText = "";
+    double answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,100 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void buttonClicked(View v) {
+        //get the contents of the button that was clicked
+        String id = (v.getResources().getResourceName(v.getId())).split("/")[1];
+        TextView t = (TextView) findViewById(R.id.CalcView);
+        Button b = (Button)v;
+        String buttonContent = b.getText().toString();
+
+        //t.setText(buttonContent);
+
+        //if it was an operation, append the operation to the display string and clear the current entry string, update text
+        if(id.equals("addition") && operation.equals("")) {
+            functionChosen(id,buttonContent);
+        }
+        else if(id.equals("subtraction") && operation.equals("")) {
+            functionChosen(id,buttonContent);
+        }
+        else if(id.equals("multiplication") && operation.equals("")) {
+            functionChosen(id,buttonContent);
+        }
+        else if(id.equals("division") && operation.equals("")) {
+            functionChosen(id,buttonContent);
+        }
+        else if(id.equals("percent") && operation.equals("")) {
+            functionChosen(id, buttonContent);
+        }
+        else if(id.equals("sign") && !currentEntry.equals("")) {
+            double d;
+            d = Double.parseDouble(currentEntry);
+            d = d*-1;
+            currentEntry = Double.toString(d);
+            //figure out how to update the view correctly
+        }
+        else if(id.equals("clear")) {
+            firstOperand = 0;
+            secondOperand = 0;
+            operation = "";
+            currentEntry = "";
+            displayText = "";
+            t.setText("0");
+        }
+        else if(id.equals("sqrt")) {
+            functionChosen(id,buttonContent);
+        }
+        //if it was equals, append "=" to the display text, perform the operation corresponding to the operation string on the operand(s)
+        else if(id.equals("equals") || !currentEntry.equals("")) {
+            displayText += currentEntry + " " + buttonContent + " ";
+            executeFunction();
+        }
+        //if it was a number or a decimal, append it to the display string and the current entry string, update text
+        else {
+            currentEntry += buttonContent;
+            t.setText(displayText + currentEntry);
+        }
+
+    }
+
+    public void executeFunction() {
+        TextView t = (TextView) findViewById(R.id.CalcView);
+        if (currentEntry != "") {
+            secondOperand = Double.parseDouble(currentEntry);
+        }
+        else {
+            secondOperand = 0.0;
+        }
+        if(operation.equals("addition")) {
+            answer = firstOperand + secondOperand;
+        }
+        else if(operation.equals("subtraction")) {
+            answer = firstOperand - secondOperand;
+        }
+        else if(operation.equals("multiplication")) {
+            answer = firstOperand * secondOperand;
+        }
+        else if(operation.equals("division")) {
+            answer = firstOperand / secondOperand;
+        }
+        else if(operation.equals("sqrt")) {
+            answer = Math.sqrt(firstOperand);
+        }
+        else{
+            t.setText("ERR");
+        }
+        displayText += answer;
+        t.setText(displayText);
+    }
+
+    public void functionChosen(String id, String buttonContent) {
+        operation = id;
+        firstOperand += Double.parseDouble(currentEntry);
+        displayText += currentEntry + " " + buttonContent + " ";
+        TextView t = (TextView) findViewById(R.id.CalcView);
+        t.setText(displayText);
+        currentEntry = "";
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
